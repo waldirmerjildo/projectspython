e719a63dc08da83a7361282fa3a180ffefd30053
waldirmerjildo <muwaldir@gmail.com>
2022-04-14 00:51:04 -0500

    modifique e codig par que elija solo numeros entre 1 y 1000

--- par_impar.py
+++ par_impar.py
@@ -2,15 +2,19 @@
 
 def par_impar():
     num = int(input("Elige un numero entre 1 y 1000:  "))
-    if num % 2 == 0:
-        print(f'El numero {num} es PAR! ¿Puedes añadir otro?')
-        pregunta()
+    if num >0 and num < 1001:
+        if num % 2 == 0:
+            print(f'El numero {num} es PAR! ¿Puedes añadir otro?')
+            pregunta()
+        else:
+            print(f'El numero {num} es IMPAR! ¿Puedes añadir otro?')
+            pregunta()
     else:
-        print(f'El numero {num} es IMPAR! ¿Puedes añadir otro?')
-        pregunta()
+        print("El numero debe ser entre 1 y 1000 please!")
+        par_impar()
     
 def pregunta():
-    letra = input("Eige s si deseas continuar o n para salir: ")
+    letra = input("Eige [s] si deseas continuar o [n] para salir: ")
     if letra == 's':
         par_impar()
     elif letra == 'n':
